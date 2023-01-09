1e991b27681dc14f4b96c04f43389f3a07870633
maroze <91451262+maroze@users.noreply.github.com>
2023-01-09 18:35:38 +0300

    09-01-2023

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Product 
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public float Cost { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                Product product1 = new Product { Type = "Phone", Cost = 20000 };
+                Product product2 = new Product { Type = "Notebook", Cost = 50000 };
+
+                // Добавление
+                db.Products.AddRange(product1, product2);
+                db.SaveChanges();
+            }
+
+            // получение
+            using (ApplicationContext db = new ApplicationContext())
+            {
+            
+                var products = db.Products.ToList();
+                Console.WriteLine("Данные после добавления:");
+                foreach (Product p in products)
+                {
+                    Console.WriteLine($"{p.Id}.{p.Type} - {p.Cost} руб");
+                }
+            }
+
+            // Редактирование
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                
+                Product product = db.Products.FirstOrDefault();
+
+                if (product != null)
+                {
+                    product.Type = "Printer";
+                    product.Cost = 54000;
+                    db.Products.Update(product);
+                    db.SaveChanges();
+                }
+               
+                Console.WriteLine("\nДанные после редактирования:");
+                var products = db.Products.ToList();
+                foreach (Product p in products)
+                {
+                    Console.WriteLine($"{p.Id}.{p.Type} - {p.Cost} руб");
+                }
+            }
+
+            // Удаление
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                
+                Product product = db.Products.FirstOrDefault();
+               
+                if (product != null)
+                {
+                   
+                    db.Products.Remove(product);
+                    db.SaveChanges();
+                }
+               
+                Console.WriteLine("\nДанные после удаления:");
+                var products = db.Products.ToList();
+                foreach (Product p in products)
+                {
+                    Console.WriteLine($"{p.Id}.{p.Type} - {p.Cost} руб");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
