b29ae54a3ade9e1124e96f958a5093a6d2820ba8
maroze <91451262+maroze@users.noreply.github.com>
2023-01-25 21:11:09 +0300

    first less

---
+++ Employees/Models/Enrollment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+
+        /// <summary>
+        /// Внешний ключ
+        /// </summary>
+        public int CourseID { get; set; }
+
+        /// <summary>
+        /// Внешний ключ
+        /// </summary>
+        public int StudentID { get; set; }
+
+        /// <summary>
+        /// Знак ? после объявления типа Grade указывает, что свойство Grade допускает значение NULL.
+        /// </summary>
+        public Grade? Grade { get; set; }
+
+        /// <summary>
+        /// Свойство навигации
+        /// </summary>
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ Employees/Program.cs
@@ -0,0 +1,45 @@
+using Employees.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "При заполнении базы данных произошла ошибка.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
