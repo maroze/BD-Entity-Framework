01638d2ae9fc4016063dd872108353e5bec4486e
maroze <91451262+maroze@users.noreply.github.com>
2023-02-27 14:22:26 +0300

    Revert "with wpf"
    This reverts commit 8218917359ab7fb41390f37ff2d61b1da92c0fbd.

--- ConsoleApp1/User.cs
+++ ConsoleApp1/User.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace BD
+namespace ConsoleApp1
 {
-    public class Product
+    public class User
     {
         public int Id { get; set; }
-        public string Type { get; set; }
-        public double Cost { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
